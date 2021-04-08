ccd573e9870b61d8025685d9e9128e5781fd5932
zenonalex <zenon_er@hotmail.com>
2021-04-07 22:20:22 -0300

    inicial

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,8 +1,27 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:teste_recursos_nativos/utils/db_util.dart';
 import '../models/place.dart';
 
 class GreatPLaces with ChangeNotifier {
   List<Place> _items = [];
+
+  Future<void> loadPlaces() async {
+    final dataList = await DbUtil.getData('places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: null,
+          ),
+        )
+        .toList();
+    notifyListeners();
+  }
 
   List<Place> get items {
     return [..._items];
@@ -14,5 +33,23 @@ class GreatPLaces with ChangeNotifier {
 
   Place itemByIndex(int index) {
     return _items[index];
+  }
+
+  void addPlace(String title, File image) {
+    final newPlace = Place(
+      id: Random().nextDouble().toString(),
+      title: title,
+      image: image,
+      location: null,
+    );
+    _items.add(newPlace);
+
+    DbUtil.insert('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+
+    notifyListeners();
   }
 }
---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+
+class ImageInput extends StatefulWidget {
+  final Function onSelectImage;
+
+  ImageInput(this.onSelectImage);
+
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  _takePicture() async {
+    print('foto');
+    final ImagePicker _picker = ImagePicker();
+    PickedFile imageFile = await _picker.getImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+
+    if (imageFile == null) return;
+
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    String fileName = path.basename(_storedImage.path);
+    final savedImage = await _storedImage.copy(
+      '${appDir.path}/$fileName',
+    );
+    widget.onSelectImage(savedImage);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 180,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          alignment: Alignment.center,
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : Text('Nenhuma imagem'),
+        ),
+        SizedBox(width: 10),
+        Expanded(
+          child: TextButton.icon(
+            onPressed: _takePicture,
+            icon: Icon(Icons.camera),
+            label: Text('Tirar foto'),
+          ),
+        )
+      ],
+    );
+  }
+}
